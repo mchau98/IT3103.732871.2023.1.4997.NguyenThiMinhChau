e3062ff2fc4cb7c59e2f3d3b85d85b47d096c67d
mchau98 <nguynminhchau98@gmail.com>
2023-10-31 22:12:44 +0700

    [fix aims]

--- Lab02/Aims.java
+++ Lab02/Aims.java
@@ -1,10 +1,12 @@
 public class Aims {
     public static void main(String[] args) {
         Cart anOrder = new Cart();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", 
+        "Roger Allers", 87, 19.95f);
         anOrder.addDigitalVideoDisc(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
+         "George Lucas", 87, 24.95f);
         anOrder.addDigitalVideoDisc(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation",18.99f);
@@ -13,7 +15,6 @@ public class Aims {
         DigitalVideoDisc dvd4 = new DigitalVideoDisc("Your name", "Romance",20.21f);
         anOrder.addDigitalVideoDisc(dvd4);
         anOrder.totalCost();
-        
         
     }
 }
